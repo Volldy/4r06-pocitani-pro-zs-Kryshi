c9859940dd3a771ee9b787c25682e356566ca131
KrySt <stejskalkryshi@gmail.com>
2019-02-05 11:28:50 +0100

    Add files via upload

--- TkinterMath.py
+++ TkinterMath.py
@@ -7,73 +7,115 @@ Created on Tue Jan 22 09:55:18 2019
 """
 from random import randint
 from tkinter import *
-  
+vysledek=0  
 class App(): 
     
     def __init__(self, master):
-        frame = Frame(master)
-        frame.pack()
         
+        #vytvoření okna
+        self.frame = Frame(master)
+        self.frame.pack()
+            
+        #vytvoření nápisu operace
+        self.Label = Label(self.frame, text=u"Vyber jakou operaci chceš provést:")
+        self.Label.grid(row = 0, column=0,columnspan=10)
         
-        self.Label = Label(frame, text=u"Operace: ")
-        self.Label.grid(row = 0, column=0,columnspan=3)
-    
-        self.button = Button(frame,text="Konec",command=master.destroy)
+        #button na ukončení
+        self.button = Button(self.frame,text="Konec",command=master.destroy)
         self.button.grid(row = 5,column=0,columnspan=2)
         
+        #okno na zadání čísla
+        self.number1entry=Entry(self.frame,width=4)
+        self.number1entry.grid(row=1,column=0)
+        
+        self.number2entry=Entry(self.frame,width=4)
+        self.number2entry.grid(row=1,column=5)
+        
+        self.Label = Label(self.frame, text=u"=")
+        self.Label.grid(row = 2, column=0,columnspan=1)
+        
+        self.vysledek=Entry(self.frame,width=4)
+        self.vysledek.grid(row=2,column=1)
+        
+        self.tlacitko = Button(self.frame,width=4, text=u"Vypočítej")
+        self.tlacitko.grid(row=2, column= 3)
+        self.radiobuttons()
+        
+    def radiobuttons(self):
+        
+        #vybrat plus
         self.var = IntVar()
-        self.plus = Radiobutton(frame, text="+",variable= self.var,value=1)
-        self.plus.grid(row=0,column=3,columnspan=1)
+        self.plus = Radiobutton(self.frame, text="+",command=self.plus,variable= self.var,value=1)
+        self.plus.grid(row=1,column=1,columnspan=1)
         
-        self.minus = Radiobutton(frame, text="-",variable= self.var,value=2)
-        self.minus.grid(row=0,column=4,columnspan=1)
+        #vybrat minus
+        self.minus = Radiobutton(self.frame, text="-",command=self.minus,variable= self.var,value=2)
+        self.minus.grid(row=1,column=2,columnspan=1)
         
-        self.times = Radiobutton(frame, text="*",variable= self.var,value=3)
-        self.times.grid(row=0,column=5,columnspan=1)
+        #vybrat násobení
+        self.times = Radiobutton(self.frame, text="*",command=self.times,variable= self.var,value=3)
+        self.times.grid(row=1,column=3,columnspan=1)
         
-        self.div = Radiobutton(frame, text="/",variable= self.var,value=4)
-        self.div.grid(row=0,column=6,columnspan=1)
-        
-        self.cislo1=Entry(frame,width=4)
-        self.cislo1.grid(row=1,column=0)
-        
-        #self.lblOperace=Label(frame,text = self.var)
-        
-        
-        
-        
-    def plus(self, x, y):
-        self.x = randint(1,99)
-        self.y = randint(0,100 -x)
-        self.result = x + y
+        #vybrat dělení
+        self.div = Radiobutton(self.frame, text="/",command=self.div,variable= self.var,value=4)
+        self.div.grid(row=1,column=4,columnspan=1)
+    
+
+        #funkce plus, která se vybere radiobuttonem plus a provede se
+    def plus(self):
+        #náhodné číslo od 1-99
+        self.number1 = randint(1,99)
+        #nejdřív první okno smaže obsah
+        self.number1entry.delete(0,5)
+        #poté do něho vloží výše generované číslo
+        self.number1entry.insert(0,str(self.number1))
+        #to stejny, ale generuje od prvního čísla do 100, aby nebylo nižší
+        self.number2 = randint(self.number1,100)
+        #zase smaže a vloží vygenerované číslo
+        self.number2entry.delete(0,5)
+        self.number2entry.insert(0,str(self.number2))
+        #vytvořím proměnnou result, která obsahuje správnou odpověď
+        self.result = self.number1 + self.number2
         return()
     
     def minus(self):
-        self.x = randint(1,99)
-        self.y = randint(0, self.x)
-        self.result = self.x - self.y
+        self.number1 = randint(1,99)
+        self.number1entry.delete(0,5)
+        self.number1entry.insert(0,str(self.number1))
+        self.number2 = randint(0, self.number1)
+        self.number2entry.delete(0,5)
+        self.number2entry.insert(0,str(self.number2))
+        self.result = self.number1 - self.number2
+        return()
         
     def times(self):
-        self.x = randint(1,9)
-        self.y = randint(1,9)
-        self.result = self.x * self.y
+        self.number1 = randint(1,9)
+        self.number1entry.delete(0,5)
+        self.number1entry.insert(0,str(self.number1))
+        self.number2 = randint(1,9)
+        self.number2entry.delete(0,5)
+        self.number2entry.insert(0,str(self.number2))
+        self.result = self.number1 * self.number2
+        return()
         
+    
     def div(self):
-        self.result = randint(1,9)
-        self.y = randint(1,9)
-        self.x = self.result * self.y
-        
-    #def vypocet(self)
-        
-        
+        self.number1 = randint(2,100)
+        self.number1entry.delete(0,5)
+        self.number1entry.insert(0,str(self.number1))
+        self.number2 = randint(2,10) 
+        self.number2entry.delete(0,5)
+        self.number2entry.insert(0,str(self.number2))
+        self.result = self.number1 * self.number2
+        return()
         
 
 
-if __name__=="__main__":
 
-    root = Tk()
-    app = App(root)
-    root.mainloop()
+
+root = Tk()
+app = App(root)
+root.mainloop()
         
 
 
